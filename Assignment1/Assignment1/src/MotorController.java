d87fae8954ecb603f9c6f396dd58e067ca627b82
Zsofia Toth <tothzsozso@gmail.com>
2018-03-07 14:52:43 +0100

    Motor: turning implemented

--- Assignment1/Assignment1/src/MotorController.java
+++ Assignment1/Assignment1/src/MotorController.java
@@ -9,6 +9,8 @@ public class MotorController {
 	private static double wheelWidth = 22;
 	private static int motorOffset = 1;
 	private static double wheelCircumference = wheelDiameter * Math.PI;
+	private static double distanceBetweenWheels = 128;
+	private static int acceleration = 2000;
 	
 	public static void logMessage(String message) {
 		LCD.drawString(message, 0, 0);
@@ -16,12 +18,18 @@ public class MotorController {
 	}
 	
 	public static void moveForward(double distance, int speed) {
+		Motor.A.setAcceleration(acceleration);
+		Motor.B.setAcceleration(acceleration);
+		
 		Motor.A.setSpeed(speed);
 		Motor.B.setSpeed(speed);
 		int degree = calculateDegree(distance);
 		Motor.A.rotateTo(degree, true);
 		Motor.B.rotateTo(degree, true);
 		while(Motor.A.isMoving()) Thread.yield();
+		
+		Motor.A.resetTachoCount();
+		Motor.B.resetTachoCount();
 	}
 	
 	private static int calculateDegree(double distance) {
@@ -30,13 +38,37 @@ public class MotorController {
 		return (int)degreeRounded;
 	}
 	
-	public static void turnLeft(double xAxis, double yAxis, int speed) {
+	public static void turn(double turningDistance, int turningDegree, String direction) {
+		turningDistance = turningDistance - wheelDistanceFromCenter;
+		int time = 2; //seconds
+		double wheelInnerSpeed = (2 * turningDistance * turningDegree) / (wheelDiameter * time);
+		double wheelOuterSpeed = (2 * turningDegree * (turningDistance + distanceBetweenWheels))/(wheelDiameter * time);
 		
+		Motor.A.setAcceleration(acceleration);
+		Motor.B.setAcceleration(acceleration);
+		
+		if(direction == "left") {
+			Motor.A.setSpeed(Math.round(wheelInnerSpeed));
+			Motor.B.setSpeed(Math.round(wheelOuterSpeed));
+		} else if (direction == "right") {
+			Motor.A.setSpeed(Math.round(wheelOuterSpeed));
+			Motor.B.setSpeed(Math.round(wheelInnerSpeed));
+		} else {
+			//TODO: refactoring
+		}
+		
+		Motor.A.forward();
+		Motor.B.forward();
+		
+		try { Thread.sleep(time*1000); } catch(Exception e) {}
+		
+		Motor.A.stop(true);
+		Motor.B.stop(true);
+		
+		Motor.A.resetTachoCount();
+		Motor.B.resetTachoCount();
 	}
 	
-	public static void turnRight(double xAxis, double yAxis, int speed) {
-		
-	}
 	
 	public static void rotate(double angle, int speed) {
 		
