6ed2656a1e6ae8861307afa040cf4d5c52ced372
Vogete <fraknoiz@gmail.com>
2018-03-07 10:26:16 +0100

    hello world program

---
+++ Assignment1/Assignment1/src/Assignment1.java
@@ -0,0 +1,14 @@
+import lejos.hardware.*;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.Motor;
+import lejos.utility.Delay;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LCD.drawString("Hello! :)", 0, 4);
+		Delay.msDelay(5000);
+	}
+
+}
