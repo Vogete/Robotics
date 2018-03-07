524905debc7a15f08d3d608863d0d6634376497d
Vogete <fraknoiz@gmail.com>
2018-03-07 11:35:36 +0100

    Settings added

--- Assignment1/Assignment1/src/Assignment1.java
+++ Assignment1/Assignment1/src/Assignment1.java
@@ -4,6 +4,9 @@ import lejos.hardware.motor.Motor;
 import lejos.utility.Delay;
 
 public class Assignment1 {
+	public double WheelDiameter = 43.2;
+	public double WheelDistanceFromCenter = 63;
+	public double WheelWidth = 22;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
